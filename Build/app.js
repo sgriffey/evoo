b81a4bb8b880d857dab05f4b5097d8bbbb8222d5
sgriffey <sbgriffi@gmail.com>
2014-08-03 11:38:44 -0700

    jquery hero juggle and others

---
+++ Build/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	var heroList = ["Browse", "Plan", "Cook", "Discover", "Serve"];
+	var delayTime = 1500;
+	var index = 0;
+	var updaterFunction = function() {
+		$("#hero__juggle").html(heroList[index]);
+		$("#hero__juggle").fadeIn(500).delay(500).fadeOut(500);
+		index = (index + 1)%heroList.length;
+	};
+	window.setInterval(updaterFunction, delayTime);
+});
